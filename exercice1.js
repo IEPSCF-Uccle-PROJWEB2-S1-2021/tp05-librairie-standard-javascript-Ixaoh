f53de985c6494c4f7e2cc9f5cb93281afacb7243
Sébastien Hals <hase1704@student.iepscf-uccle.be>
2020-10-10 11:52:12 +0200

    tp05 done

--- exercice1.js
+++ exercice1.js
@@ -1,3 +1,29 @@
 function chuckNorris(msg) {
-  return "";
+  let msgBinaire = "";
+
+  for (i in msg){
+    msgBinaire += msg[i].charCodeAt(0).toString([2]) + " ";
+  }
+
+  let tabMsg = msgBinaire.split("");
+  let msgUnaire ="";
+  if (tabMsg[0] == '1'){
+    msgUnaire += "0 0";
+  }
+  else {
+    msgUnaire += "00 0";
+  }
+
+  for (let i = 1; i<tabMsg.length; i++){
+    if(tabMsg[i] == tabMsg[(i-1)]){
+      msgUnaire+= "0";
+    } else if (tabMsg[i] == '1'){
+      msgUnaire += " 0 0";
+    } else {
+      msgUnaire += "00 0";
+    }
+  }
+  return msgUnaire;
 }
+console.log(chuckNorris("C"));
+console.log(chuckNorris("CC"));
--- exercice2.js
+++ exercice2.js
@@ -1,3 +1,29 @@
 function analyzeTemperature(quantity, data) {
-  return 0;
+
+  let tabData = data.split(" ");
+  let valProche = 0;
+
+
+  if (quantity >=0 && quantity <=10000){
+    for (let i = 0;i<tabData.length;i++){
+      if(valProche ===0){
+        valProche = tabData[i];
+      }
+      else if (tabData[i] > 0 && tabData[i] <= Math.abs(valProche)){
+        valProche = tabData[i];
+      }
+      else if (tabData[i] < 0 && -tabData[i] <= Math.abs(valProche)){
+        valProche = tabData[i];
+      }
+
+    }
+  return valProche;
+  }
+  else {
+    return "Erreur : La quantité comprise entre 0 et 10.000 max";
+  }
 }
+
+let testData = '2 -5 -7 9 3';
+
+console.log(analyzeTemperature(1000,testData));
